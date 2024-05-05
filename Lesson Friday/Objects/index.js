88bf354f35c3a27c0066827747832084a38d4c9d
Swandile <sandilesnene01@gmail.com>
2024-05-05 14:30:49 +0200

    🚧 Created data collection functionality for the form

--- Lesson Friday/Objects/index.js
+++ Lesson Friday/Objects/index.js
@@ -1,4 +1,3 @@
-
 // myDatabase = {}
 //
 // function UserAccount(username, password, confirmPassword, email) {
@@ -38,43 +37,85 @@
 // const data = {}
 
 
+// myDatabase = {};
+//
+// function UserAccount (username, password, confirmPassword, email) {
+//     this.username = username;
+//     this.password = password;
+//     this.confirmPassword = confirmPassword;
+//     this.email = email;
+//     this.cart = []
+//
+//     this.addToDb = function () {
+//         myDatabase[this.email] = {username: this.username, email: this.email};
+//     }
+//
+//     this.deleteUserAccount = function() {
+//         delete myDatabase[this.email];
+//     }
+//
+//     this.addToCart = function (products){
+//         this.cart.push(products)
+//     }
+//
+//     this.viewCart = function (){
+//         return this.cart;
+//     }
+// }
+//
+// console.log(myDatabase)
+// let newUser1 = userAccount1 = new UserAccount("Bro", "Code", "Code", "bro1@gmail.com");
+// let newUser2 = userAccount2 = new UserAccount("Bro2", "Code", "Code", "bro2@gmail.com");
+//
+// newUser1.addToCart("Potatoes");
+// newUser2.addToCart("Tomatoes");
+//
+// newUser1.addToDb();
+// newUser2.addToDb();
+// console.log(myDatabase)
+// console.log(myDatabase[""])
+
+
+const signUpBtn = document.getElementById("sign__up__btn__id");
 myDatabase = {};
+console.log(myDatabase);
 
-function UserAccount (username, password, confirmPassword, email) {
-    this.username = username;
-    this.password = password;
-    this.confirmPassword = confirmPassword;
-    this.email = email;
-    this.cart = []
+signUpBtn.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    this.addToDb = function () {
-        myDatabase[this.email] = {username: this.username, email: this.email};
+    let username = document.getElementById("username__id").value;
+    let email = document.getElementById("email__id").value;
+    let password = document.getElementById("password__id").value;
+    let confirmPassword = document.getElementById("confirm__password__id").value;
+    let confirmPasswordLabel = document.getElementById("confirm__password__label");
+
+    function UserAccount(username, email, password, confirmPassword) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.addToDB = function () {
+            myDatabase[this.email] = {username: this.username, email: this.email, password: this.password};
+        }
     }
 
-    this.deleteUserAccount = function() {
-        delete myDatabase[this.email];
+    if (confirmPassword !== password) {
+        confirmPasswordLabel.classList.add("red");
+        confirmPasswordLabel.textContent = "Passwords don't match:";
+    } else {
+        let newUser = new UserAccount(username, email, password, confirmPassword);
+        confirmPasswordLabel.classList.remove("red");
+        confirmPasswordLabel.classList.add("green");
+        confirmPasswordLabel.textContent = "Passwords match";
+        newUser.addToDB();
+        console.log(myDatabase);
+
     }
+});
 
-    this.addToCart = function (products){
-        this.cart.push(products)
-    }
 
-    this.viewCart = function (){
-        return this.cart;
-    }
-}
 
-console.log(myDatabase)
-let newUser1 = userAccount1 = new UserAccount("Bro", "Code", "Code", "bro1@gmail.com");
-let newUser2 = userAccount2 = new UserAccount("Bro2", "Code", "Code", "bro2@gmail.com");
 
-newUser1.addToCart("Potatoes");
-newUser2.addToCart("Tomatoes");
-
-newUser1.addToDb();
-newUser2.addToDb();
-console.log(myDatabase)
-console.log(myDatabase[""])
 
 
 

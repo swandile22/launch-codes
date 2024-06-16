6101d3389c90ae044b189f34b542e3e8f59a7206
Swandile <sandilesnene01@gmail.com>
2024-06-16 19:18:55 +0200

    ⚒ Created a feature that displays the total based on the cart items and their quantity

--- Lesson Friday/Objects/cartPage/cart.js
+++ Lesson Friday/Objects/cartPage/cart.js
@@ -43,45 +43,6 @@ const cartItemCard = (image, title, description, price) => {
     `
 }
 
-// function CartItemObject(image, productTitle, description, price) {
-//     this.image = image;
-//     this.productTitle = productTitle;
-//     this.description = description;
-//     this.price = price;
-//
-//     this.getImage = function () {
-//         return this.image;
-//     }
-//
-//     this.getTitle = function () {
-//         return this.productTitle;
-//     }
-//
-//     this.getDescription = function () {
-//         return this.description;
-//     }
-//
-//     this.getPrice = function () {
-//         return this.price;
-//     }
-//
-//     let setImage = (newImage) => {
-//         this.image = newImage;
-//     }
-//
-//     let setTitle = (newTitle) => {
-//         this.title = newTitle;
-//     }
-//
-//     let setDescription = (newDescription) => {
-//         this.description = newDescription;
-//     }
-//
-//     let setPrice = (newPrice) => {
-//         this.price = newPrice;
-//     }
-// }
-
 const cartItemsList = [];
 
 createCartObject(cartItems);
@@ -94,8 +55,6 @@ cartItemsList.forEach((item) => {
         item.getDescription(),
         item.getPrice());
 });
-
-const removeItemsAncestor = document.querySelectorAll(".cart__page__product__section__product__remove__group");
 
 cartPageContent.addEventListener("click", (event) => {
     if (event.target && event.target.closest(".cart__page__product__section__product__remove__group")) {
@@ -114,14 +73,55 @@ cartPageContent.addEventListener("click", (event) => {
     }
 });
 
-// removeItems.forEach((item) => {
-//     item.addEventListener("click", () => {
-//         console.log("I got clicked");
-//         // const index = cartItemsList.indexOf(item);
-//         // if (index > -1) {
-//         //     cartItemsList.splice(index, 1);
-//         //     console.log(cartItemsList);
-//         // }
+document.addEventListener("DOMContentLoaded", () => {
+    const cartItems = document.querySelectorAll(".cart__page__product__section__product");
+    let total = 0;
+
+    cartItems.forEach((item) => {
+        const priceElement = item.querySelector(".cart__page__product__section__product__price__group__price");
+        const qtyElement = item.querySelector(".cart__page__product__section__product__qty");
+
+        const cartItem = {
+            price: parseFloat(priceElement.innerText.replace("R", "")),
+            quantity: parseInt(qtyElement.value)
+        };
+
+        qtyElement.addEventListener("change", (event) => {
+            cartItem.quantity = parseInt(event.target.value);
+            updateTotal();
+        });
+
+        item.dataset.cartItem = JSON.stringify(cartItem);
+    });
+
+    const updateTotal = () => {
+        total = 0;
+        cartItems.forEach((item) => {
+            const cartItem = JSON.parse(item.dataset.cartItem);
+            total += cartItem.price * cartItem.quantity;
+        });
+
+        const totalFormatted = total.toFixed(2);
+        const checkoutTotalPrice = document.getElementById("cart__page__section__checkout__total__group__price__id");
+
+        if (checkoutTotalPrice) {
+            checkoutTotalPrice.innerHTML = `Total R: ${totalFormatted}`;
+        } else {
+            console.log(`No element id matches "cart__page__section__checkout__total__group__price__id"`);
+        }
+    };
+
+    updateTotal();
+});
+
+
+// const cartCards = document.querySelectorAll(".cart__page__product__section__product__qty");
+// const cartItemPrice = document.querySelectorAll(".cart__page__product__section__product__price__group__price");
+//
+//
+// cartCards.forEach((card) => {
+//     card.addEventListener("change", (event) => {
+//         console.log(card.value);
 //     })
 // })
 
--- Lesson Friday/Objects/homePage/HomePage.js
+++ Lesson Friday/Objects/homePage/HomePage.js
@@ -45,45 +45,6 @@ const generateCard = (image, title, description, price) => {
  * @price [number]  accepts a number for the price element
  * */
 
-// function ProductObject(image, title, description, price) {
-//     this.image = image;
-//     this.title = title;
-//     this.description = description;
-//     this.price = price;
-//
-//     this.getImage = function () {
-//         return this.image;
-//     }
-//
-//     this.getTitle = function () {
-//         return this.title;
-//     }
-//
-//     this.getDescription = function () {
-//         return this.description;
-//     }
-//
-//     this.getPrice = function () {
-//         return this.price;
-//     }
-//
-//     let setImage = (newImage) => {
-//         this.image = newImage;
-//     };
-//
-//     let setTitle = (newTitle) => {
-//         this.title = newTitle;
-//     };
-//
-//     let setDescription = (newDescription) => {
-//         this.description = newDescription;
-//     };
-//
-//     let setPrice = (newPrice) => {
-//         this.price = newPrice;
-//     };
-//
-// }
 
 const productsList = [];
 
